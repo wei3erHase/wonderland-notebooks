d0d7b02801c4f558ee40d7b4a6158c02d77b3e6e
wei3erHase <wei3erhase@defi.sucks>
2021-11-13 10:38:08 +0100

    fix: common makeASwap

--- utils/common.js
+++ utils/common.js
@@ -9,12 +9,15 @@ const { ethers, web3, artifacts } = require('hardhat');
 
 const makeASwap = async (provider, fromToken, toToken, receiver, fee, amount) => {
   uniRouter = await ethers.getContractAt('ISwapRouter', '0xE592427A0AEce92De3Edee1F18E0157C05861564');
+  token = await ethers.getContractAt('ERC20', fromToken);
+
+  await token.connect(provider).approve(uniRouter.address, amount)
 
   const blockTimestamp = (await ethers.provider.getBlock('latest')).timestamp;
 
   return await uniRouter
     .connect(provider)
-    .exactInputSingle([fromToken, toToken, 10000, receiver, blockTimestamp + 100, amount, toUnit(0.0001), 0]);
+    .exactInputSingle([fromToken, toToken, fee, receiver, blockTimestamp + 100, amount, toUnit(0.0001), 0]);
 };
 
 exports.makeASwap = makeASwap;
